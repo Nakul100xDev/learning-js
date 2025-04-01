7005ee6eb692050c9286674974a8f97f103851eb
Nakul100xDev <205391583+Nakul100xDev@users.noreply.github.com>
2025-04-01 10:16:52 +0530

    datatypes

---
+++ 01_basics/02_dataTyes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+console.log(3
+    +
+    3); console.log("Nakul");
+
+// we reduced code readability here, this is a DON'T
+
+let name = "Nakul"
+let age = 19
+let isLoggedIn = false
+
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// bolean -> true or false
+// null -> standalone value (empty, but not undefined)
+// undefined ->
+// symbol -> unique
+
+// object
+
+console.log(typeof"hitesh");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
