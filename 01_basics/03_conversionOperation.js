d7778112723835a7b1efee4782346ca4552f441b
Nakul100xDev <205391583+Nakul100xDev@users.noreply.github.com>
2025-04-01 13:11:44 +0530

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score) //Datatype we want to assign
+// console.log(typeof(valueInNumber));
+
+// // pure number 123232 -> number
+// // unpure number 33abcasd NaN
+// // null -> to 0
+// // undefinded -> NaN
+// // Boolean True -> 1
+// // Boolean False -> 0
+
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber) 
+
+// //NaN -> Not a Number
+
+// let isLoggedIn = "nakul"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// // 1 -> true; 0 -> false
+// // "" -> false; "nakul" -> true
+
+let somenumber = 34
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
